590f372d423cbf103f697bb6f492d9d2d4e4380b
alexseyakabamba-lgtm <alexseyakabamba@gmail.com>
2026-07-07 08:50:58 +0200

    Create or alter fact_sales view for sales data
    Creates or alters the fact_sales view to link sales transactions with customer and product dimensions.

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -96,3 +96,38 @@ LEFT JOIN silver.erp_px_cat_g1v2 pc
 WHERE pn.prd_end_dt IS NULL;        -- Keep only the most recent products.
 
 GO
+
+-- ============================================================================
+-- Create Fact: gold.fact_sales
+-- ============================================================================
+-- Creates the central sales fact table by linking sales transactions
+-- to the customer and product dimensions.
+-- ============================================================================
+
+CREATE OR ALTER VIEW gold.fact_sales AS
+
+SELECT
+    sd.sls_ord_num  AS order_number,         -- Order Information
+
+    pr.product_key,         -- Dimension Keys
+    cu.customer_key,
+
+    sd.sls_order_dt AS order_date,       -- Dates
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+
+    sd.sls_sales     AS sales_amount,       -- Measures
+    sd.sls_quantity  AS quantity,
+    sd.sls_price     AS price
+
+FROM silver.crm_sales_details sd
+
+LEFT JOIN gold.dim_products pr      -- Link to Product Dimension
+    ON sd.sls_prd_key = pr.product_number
+
+LEFT JOIN gold.dim_customers cu     -- Link to Customer Dimension
+    ON sd.sls_cust_id = cu.customer_id;
+
+GO
+
+
